2de2d1559bfc3afba5d7a2016d732e008d2a0954
Sammy Dallal <sammydallal@gmail.com>
2017-02-03 13:28:42 -0800

    added GET /todo/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +28,24 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	})
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		res.status(404).send();
+		return console.log('ID not valid');
+	};
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			res.status(404).send()
+			return console.log('no todo found');
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+
 });
 
 app.listen(8000, () => {
